8b4989d9be328f0ab8fac2293387c49e4ddd3691
BenoitFayolle <benoit.fayolle@hotmail.fr>
2018-03-22 15:39:31 +0000

    update ui.R
    fixed panel with map on the left
    Many new models
    Goodness of fit models added
    logy scale added

--- ui.R
+++ ui.R
@@ -1,47 +1,37 @@
-#
-# This is the user-interface definition of WindProfileViewer. You can
-# run the application by clicking 'Run App' above.
-#
-# 
 
 library(shiny)
 library(leaflet)
+library(tidyverse)
 
 # Define UI for application that draws a histogram
 shinyUI(fluidPage(
-    
-    tabPanel("Wind Profile Viewer",
-             div(class="outer",
-                 # 
-                 # tags$head(
-                 #     # Include our custom CSS
-                 #     includeCSS("styles.css"),
-                 #     includeScript("gomap.js")
-                 # ),
-                 
-                 # If not using custom CSS, set height of leafletOutput to a number instead of percent
-                 leafletOutput("map"),#, width="800", height="600"
-                 
-                 # Shiny versions prior to 0.11 should use class = "modal" instead.
-                 absolutePanel(id = "controls", class = "panel panel-default", fixed = TRUE,
-                               draggable = TRUE, top = "auto", left = "auto", right = 20, bottom = 0,
-                               width = 600, height = "auto",
-                               
-                               h4("Fitting the wind distribution"),
-                               h5("Click any green circle!"),
-                               plotOutput("binsHist", height = 250)
-                               
-                 )
-                 
-             ),
-             # 
-             tags$div(id="cite",
-                      'Data source ', tags$em('NOAA WMO Resolution 40')
-             ),
-             downloadButton("downloadData", "Download"),
-             helpText(   a("Click here for documentation",
-                           href="http://rpubs.com/bfayolle/WeibullDistAndRealWind")
-             )
-             
+
+    sidebarLayout(
+                  sidebarPanel(
+                      leafletOutput("map",height=300),
+                      checkboxGroupInput("models","Models",
+                                         choices=list("Rayleigh-Weibull","Weibull M1 & M2","Weibull M1 & M3","WAsP"),
+                                         selected=list("Rayleigh-Weibull","Weibull M1 & M2","Weibull M1 & M3","WAsP")),
+                      checkboxGroupInput("goodness.method","Goodness of fit",
+                                         choices=list("R squared","Cramer-Von Mises","Anderson-Darling",
+                                                      "RTAD","RTAD2"),
+                                         selected = "Cramer-Von Mises"),
+                      checkboxInput("logy","Log Y-Axis",value = F)
+                  ),
+                  mainPanel(
+                      h2("Wind Profile Viewer"),
+                      plotOutput("binsHist"),
+                      h4("Fitting the wind distribution"),
+                      h5("Click any green circle!"),
+                      tags$div(id="cite",
+                               'Data source ', tags$em('NOAA WMO Resolution 40')
+                      ),
+                      downloadButton("downloadData", "Download"),
+                      helpText(   a("Click here for documentation",
+                                    href="http://rpubs.com/bfayolle/WeibullDistAndRealWind")
+                      )
+                  )
+
     )
-))
+)
+)
